361cb2e107f0a5bacae8b3d830060577997e30ba
Ross Mawdsley <ross.mawdsley@clearskylogic.com>
2024-08-05 14:30:42 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cat Match',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Cat Match Home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(widget.title),
+      ),
+  body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5, // Number of columns
+            crossAxisSpacing: 4.0, // Spacing between columns
+            mainAxisSpacing: 4.0, // Spacing between rows
+          ),
+          itemCount: 25, // Number of items (5x5 grid)
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text(
+                  '$index',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            );
+          },
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
