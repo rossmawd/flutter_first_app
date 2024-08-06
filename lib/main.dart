bc119155e68c51611bd2b3f8cfa26c662d6e90b5
Ross Mawdsley <ross.mawdsley@clearskylogic.com>
2024-08-06 15:45:29 +0100

    changed title to just be Cat Match

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Cat Match Home page'),
+      home: const MyHomePage(title: 'Cat Match'),
     );
   }
 }
